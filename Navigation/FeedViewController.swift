9616229556f075e2dfc29a1ef1dbbffb83cafdaa
Elena Stegnina <elena.safina@icloud.com>
2022-11-23 23:01:01 +0500

    Added 2 view controllers and tab bar controller. Removed main storyboard and unused view controller

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Стегнина Елена on 23.11.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Заголовок"
+        self.view.backgroundColor = UIColor.green
+        
+    }
+
+
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Стегнина Елена on 23.11.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Заголовок2"
+        self.view.backgroundColor = UIColor.blue
+    }
+    
+
+
+}
