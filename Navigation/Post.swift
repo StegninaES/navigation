821c69969a4356047823cb4de02aa9d4757e6821
Elena Stegnina <elena.safina@icloud.com>
2022-12-15 00:11:58 +0500

    Added table view.

--- Navigation/Post.swift
+++ Navigation/Post.swift
@@ -9,4 +9,10 @@ import Foundation
 
 struct Post {
     var title: String
+    var author: String
+    var description: String
+    var image: String
+    var likes: Int
+    var views: Int
+    
 }
--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -9,27 +9,62 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    var profileHeaderView: ProfileHeaderView = ProfileHeaderView(frame: .zero)
+    var posts: [Post] = []
+    var myTabelView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
+        createPosts()
+        createTable()
+        setupTable()
     }
     
-    private func setupView() {
-        self.view.backgroundColor = .lightGray
-        let tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 0)
-        self.tabBarItem = tabBarItem
-        self.view.addSubview(profileHeaderView)
+    func createPosts() {
+        let post1 = Post(title: "Lorem ipsum dolor sit amet", author: "Author1", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", image: "Image 1", likes: 3, views: 3)
+        let post2 = Post(title: "Post2", author: "Author2", description: "Description2", image: "Image 1", likes: 3, views: 3)
+        let post3 = Post(title: "Post3", author: "Author3", description: "Description3", image: "Image 1", likes: 3, views: 3)
+        let post4 = Post(title: "Post4", author: "Author4", description: "Description4", image: "Image 1", likes: 3, views: 3)
+        posts.insert(post1, at: 0)
+        posts.insert(post2, at: 1)
+        posts.insert(post3, at: 2)
+        posts.insert(post4, at: 3)
     }
     
-    override func viewWillLayoutSubviews() {
-        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
-        profileHeaderView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        profileHeaderView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        profileHeaderView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    func createTable() {
+
+        self.myTabelView = UITableView(frame: view.bounds, style: .plain)
+        myTabelView.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.identifier)
+        self.myTabelView.delegate = self
+        self.myTabelView.dataSource = self
+        
+        view.addSubview(myTabelView)
+    }
+    
+    private func setupTable() {
+        myTabelView.translatesAutoresizingMaskIntoConstraints = false
+        myTabelView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor).isActive = true
+        myTabelView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor).isActive = true
+        myTabelView.centerXAnchor.constraint (equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        myTabelView.centerYAnchor.constraint (equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
     }
 }
 
+extension ProfileViewController: UITableViewDataSource, UITableViewDelegate  {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier) as! PostTableViewCell
+        
+        cell.post = posts[indexPath.row]
+            
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return myTabelView.bounds.height
+    }
+}
 
